4362ecc3b2e97df0635920e7c12c01b51835f6d8
Pradeep1234a <93538365+Pradeep1234a@users.noreply.github.com>
2021-12-10 08:15:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -286,7 +286,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('Update Channel', url='https://t.me/LXRYOUTUBERYT'),
+                    InlineKeyboardButton('Update Channel', url='https://t.me/pradeep1234a'),
                 ]
                 ]
             await query.message.edit(text="<b>Developer : <a href='https://t.me/malayalammoviesmmms'>꧁LXR=AMAL࿐</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\n Update Channel : <a href='https://t.me/LXRYOUTUBERYT'>ErrorXBotz</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
